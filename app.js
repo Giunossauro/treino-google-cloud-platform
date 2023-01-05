63619bd4fdb4720aafe1892fbd20a3f8e401f8de
Giuliano Morelli <giuandroide@gmail.com>
2023-01-05 17:31:19 -0300

    chore: intentionally MEGA breaks the build

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
+\z
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -34,8 +34,8 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  ress.status(err.status || 500);
+  res.rendder('error');
 });
 
 module.exports = apps;
